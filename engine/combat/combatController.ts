1f4a20d43e1fab9835502680307e9101e4f05794
Myztikal125 <shadowjuice6969@gmail.com>
2026-08-07 22:00:49 -0500

    Connect combat runtime to approved Rules data

--- engine/combat/combatController.ts
+++ engine/combat/combatController.ts
@@ -1,10 +1,19 @@
 import { CombatState } from "./combatState";
+import { RulesRuntime } from "../rules/rulesRuntime";
 
 export class CombatController {
 
   state = new CombatState();
 
+  constructor(private readonly rules?: RulesRuntime) {}
+
   start(enemies: any[]) {
+
+    if (this.rules && !this.rules.isApproved("Combat Rounds")) {
+      throw new Error(
+        "Combat is unavailable because the Combat Rounds system is not approved in the Rules Bible."
+      );
+    }
 
     this.state.start(enemies);
 
