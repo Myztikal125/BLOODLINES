93d42eb1e8ffceac7d8e2e4a7be0d2fd844d9fe3
Myztikal125 <shadowjuice6969@gmail.com>
2026-08-19 08:38:15 -0500

    chore(cleanup): remove obsolete debug script

--- debug-wizard.ts
+++
@@ -1,9 +0,0 @@
-import { createCharacter } from "./src/engine/characterFactory";
-
-const wizard = createCharacter({
-  name: "Gandalf",
-  classId: "wizard",
-  level: 1
-});
-
-console.log(JSON.stringify(wizard, null, 2));
